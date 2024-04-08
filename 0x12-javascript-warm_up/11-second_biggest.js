2690a97fcdb1c41b7b3030c7f63a33adf36c5b07
Jaaf4r <jaafar.bouhnine@gmail.com>
2024-04-08 23:41:07 +0000

    js ez

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const args = process.argv.slice(2).map(Number).sort();
-  for (let i = args.length - 2; i >= 0; i--) {
-    if (args[i] !== args[args.length - 1]) {
-      console.log(args[i]);
-      break;
-    }
-  }
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
